7af7ea3e0368613f19c0daaf1d44c078951a2648
Vanessa Su <vnessa.su@gmail.com>
2021-04-12 15:01:39 -0400

    WIP: adding vowelCounter method
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>
    Co-authored-by: Ryan Walker <ryandanielwalker@gmail.com>

--- src/business_logic.js
+++ src/business_logic.js
@@ -10,3 +10,23 @@ Entry.prototype.wordCounter = function () {
   const wordArray = this.content.split(" ");
   this.wordCount= wordArray.length
 }
+
+Entry.prototype.vowelCounter = function () {
+  const vowelArray = this.content.split("");
+  //string of vowels
+  //for loop of some sort for each letter
+  //check each if vowel string includes that letter
+  //regex: ([aeiouAEIOU])
+  if (vowelArray.includes
+
+
+    const vowels = ["a", "e", "i", "o", "u"]
+    const wordArray = this.content.split(" ")
+    wordArray.forEach(function(element){
+      vowels.forEach(function(vowel){
+        if (element.includes(vowel))
+
+      })
+    })
+}
+
